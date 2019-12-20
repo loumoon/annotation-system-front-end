6427a58e6d44196f928833814a67ba08ba131cdd
loumoon <3026775966@qq.com>
2019-12-20 21:48:36 +0800

    feat: annotation page for student

--- mobile/js/panpan.js
+++ mobile/js/panpan.js
@@ -18,27 +18,3 @@ $("#managed").click(function () {
 $("#me").click(function () {
   window.location.href = "me.html";
 })
-
-//清除过期localstorage函数
-$(function () {
-  $.ajax({
-    url: 'http://212.64.11.56:8080/EAnnotation/getCurrentUser',
-    type: 'post',
-    dataType: 'jsonp',
-    xhrFields: {
-      withCredentials: true
-    },
-    crossDomain: true,
-    success: function (data) {
-      if (data == 0) {
-        localStorage.id = -1;
-        localStorage.classid = null;
-        localStorage.usertype = null;
-        console.log("清除localstorage")
-      }
-      console.log(localStorage.usertype)
-      console.log(localStorage.id) 
-    },
-    error: function () {}
-  });
-});
